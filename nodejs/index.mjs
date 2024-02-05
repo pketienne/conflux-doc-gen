aa377731c12816ef3ae5f555958d3c1b0433c3df
Patrick K. Etienne <git@paradox.limited>
2024-02-04 22:04:16 -0500

    New sample json taken from Retool. Added fs library. Modified index.mjs for local execution.

--- nodejs/index.mjs
+++ nodejs/index.mjs
@@ -1,94 +1,85 @@
-// import puppeteer from 'puppeteer-core'
-// import chromium from '@sparticuz/chromium'
-// import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import puppeteer from 'puppeteer-core'
+import chromium from '@sparticuz/chromium'
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import { promises as fs } from 'fs'
 
-// const region = process.env.AWS_REGION
-
-// export const handler = async (event) => {
-// 	const url = 'https://conflux-document-generator.s3.us-east-2.amazonaws.com/templates/invoice-1.html'
-
-// 	const client = new S3Client(config);
-// 	const input = { // ListObjectsV2Request
-// 		Bucket: "conflux-document-generator", // required
-// 		EncodingType: "url",
-// 	};
-// 	const command = new ListObjectsV2Command(input);
-// 	const response = await client.send(command);
-
-// 	try {
-// 		const browser = await puppeteer.launch({
-// 			args: chromium.args,
-// 			defaultViewport: chromium.defaultViewport,
-// 			executablePath: await chromium.executablePath(),
-// 			headless: chromium.headless,
-// 			ignoreHTTPSErrors: true,
-// 		});
-// 		const page = await browser.newPage();
-// 		await page.goto(url, { waitUntil: "networkidle0" });
-// 		await page.emulateMediaType("screen");
-// 		var PDFOptions = { printBackground: true, format: 'A4' }
-// 		if (!region) {
-// 			PDFOptions.path = '../result.pdf'
-// 		}
-// 		await page.evaluate(() => {
-// 			const location = 'div.table-responsive table tbody tr'
-// 			let dom = document.querySelectorAll(location)
-// 			dom.forEach(e => e.remove())
-// 		})
-// 		const pdf = await page.pdf(PDFOptions);
-// 		console.log(pdf)
-// 		await page.close();
-// 		await browser.close();
-// 		return {
-// 			statusCode: 200,
-// 			headers: { 'Content-Type': 'application/pdf' },
-// 			body: pdf.toString('base64'),
-// 			isBase64Encoded: true,
-// 			// https://www.google.com/search?q=lambda+nodejs+pdf+response+how+to&oq=lambda+nodejs+pdf+response+how+to&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQLhhA0gEINjg4MmowajGoAgCwAgA&sourceid=chrome&ie=UTF-8
-// 			// https://medium.com/@keshavkumaresan/generating-pdf-documents-within-aws-lambda-with-nodejs-and-puppeteer-46ac7ca299bf
-// 			// https://oxylabs.io/blog/curl-send-headers
-// 			// https://stackoverflow.com/questions/45348580/aws-lambda-fails-to-return-pdf-file
-// 		}
-// 	} catch (error) {
-// 		return {
-// 			statusCode: 500,
-// 			body: error,
-// 		}
-// 	}
-// };
-
-// if(!region) {
-// 	handler({})
-// };
-
-
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
-
+const region = process.env.AWS_REGION
 
 export const handler = async (event) => {
-	const client = new S3Client({});
-	const command = new ListObjectsV2Command({
-		Bucket: "conflux-doc-gen",
-		// The default and maximum number of keys returned is 1000. This limits it to
-		// one for demonstration purposes.
-		// MaxKeys: 1,
-	});
+	if (!region) { var event = await load_sample_json(event) }
+
+	const bucket = 'conflux-doc-gen'
+	const url_base = `https://${bucket}.s3.us-east-2.amazonaws.com`
+	const template = `templates/${event.document}-${event.template}`
+	let file_name = 'result'
 
 	try {
-		let isTruncated = true;
-		console.log("Your bucket contains the following objects:\n");
-		let contents = "";
+		const browser = await puppeteer.launch({
+			args: chromium.args,
+			defaultViewport: chromium.defaultViewport,
+			executablePath: await chromium.executablePath(),
+			headless: chromium.headless,
+			ignoreHTTPSErrors: true,
+		})
 
-		while (isTruncated) {
-			const { Contents, IsTruncated, NextContinuationToken } =
-			await client.send(command);
-			const contentsList = Contents.map((c) => ` • ${c.Key}`).join("\n");
-			contents += contentsList + "\n";
-			isTruncated = IsTruncated;
-			command.input.ContinuationToken = NextContinuationToken;
+		const page = await browser.newPage();
+
+		await page.goto( `${url_base}/${template}.html`, { waitUntil: 'networkidle0' });
+		await page.emulateMediaType("screen");
+
+		var PDFOptions = { printBackground: true, format: 'A4' }
+		if (!region) { PDFOptions.path = `../${file_name}.pdf` }
+
+		await page.evaluate(() => {
+			const location = 'div.table-responsive table tbody tr'
+			let dom = document.querySelectorAll(location)
+			dom.forEach(e => e.remove())
+		})
+
+		const pdf = await page.pdf(PDFOptions);
+		
+		await page.close();
+		await browser.close();
+
+		if(region) {
+			const client = new S3Client({});
+			const date = `${new Date().toISOString()}`
+			const hexidecimal = (Math.random() * 0xfffff * 10000000000000)
+			const obfuscation = hexidecimal.toString(16).slice(0,16)
+			const file_name = `${date}-${obfuscation}`
+			const command = new PutObjectCommand({
+				Bucket: "conflux-doc-gen",
+				Key: `${file_name}.pdf`,
+				Body: pdf,
+			});
+
+			try {
+				const response = await client.send(command);
+			} catch (err) {
+				console.error(err);
+			}
 		}
-		console.log(contents);
-	} catch (err) {
-		console.error(err);
+
+		return {
+			statusCode: 200,
+			headers: { 'Content-Type': 'application/json' },
+			body: { url: `${url_base}/${file_name}` },
+		}
+
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: error,
+		}
 	}
-};
+}
+
+async function load_sample_json(document_type) {
+	let data = await fs.readFile(`../api/${document_type}.json`, { encoding: 'utf-8' })
+	let json = JSON.parse(data)
+	return json
+}
+
+if(!region) {
+	handler('invoice')
+}
