2277f4f2b940c42e013802b2ce9b05c7ca4c15da
Patrick K. Etienne <github@pketienne.me>
2024-02-06 15:41:19 -0500

    Discovered that Puppeteer does not have the capability to inject new elements into the existing dom.

--- nodejs/index.mjs
+++ nodejs/index.mjs
@@ -8,7 +8,7 @@ const region = process.env.AWS_REGION
 export const handler = async (event) => {
 	const bucket = 'conflux-doc-gen'
 	const url_base = `https://${bucket}.s3.us-east-2.amazonaws.com`
-	const template = `templates/${event.document}-${event.template}`
+	const template = `../templates/${event.document}-${event.template}`
 	const date = `${new Date().toISOString()}`
 	const hexidecimal = (Math.random() * 0xfffff * 10000000000000)
 	const obfuscation = hexidecimal.toString(16).slice(0,16)
@@ -24,37 +24,83 @@ export const handler = async (event) => {
 		})
 		const page = await browser.newPage()
 		await page.goto(`${url_base}/${template}.html`, {waitUntil: 'networkidle0'})
+		// await page.goto(`${url_base}/${template}.html`, {waitUntil: 'domcontentloaded'})
 		await page.emulateMediaType('screen')
 		var PDFOptions = {printBackground: true, format: 'A4'}
 		if (!region) {PDFOptions.path = 'result.pdf'}
 
-		page.on('console', async (msg) => {
-			const msgArgs = msg.args();
-			for (let i = 0; i < msgArgs.length; ++i) {
-				console.log(await msgArgs[i].jsonValue());
-			}
-		})
+		page.on('console', msg => console.log('BROWSER LOG:', msg.text()));
 
 		await page.evaluate((event) => {
 			let instructions = [
-				{selector: 'span#pke-invoice-number', innerHTML: event.invoice.number},
-				{selector: 'span#pke-invoice-date',	innerHTML: event.invoice.date.slice(0,10)},
-				{selector: 'h2#pke-client-person', innerHTML: event.client.person},
-				{selector: 'span#pke-client-company', innerHTML: event.client.company},
-				{selector: 'span#pke-client-email', innerHTML: event.client.email},
-				{selector: 'span#pke-client-address', innerHTML: event.client.address},
-				{selector: 'h2#pke-contractor-person', innerHTML: event.contractor.person},
-				{selector: 'span#pke-contractor-company', innerHTML: event.contractor.company},
-				{selector: 'span#pke-contractor-email', innerHTML: event.contractor.email},
-				{selector: 'span#pke-contractor-address', innerHTML: event.contractor.address},
+				{selector: 'span#mdb-invoice-number', innerHTML: event.invoice.number},
+				{selector: 'span#mdb-invoice-date',	innerHTML: event.invoice.date.slice(0,10)},
+				{selector: 'h2#mdb-client-person', innerHTML: event.client.person},
+				{selector: 'span#mdb-client-company', innerHTML: event.client.company},
+				{selector: 'span#mdb-client-email', innerHTML: event.client.email},
+				{selector: 'span#mdb-client-address', innerHTML: event.client.address},
+				{selector: 'h2#mdb-contractor-person', innerHTML: event.contractor.person},
+				{selector: 'span#mdb-contractor-company', innerHTML: event.contractor.company},
+				{selector: 'span#mdb-contractor-email', innerHTML: event.contractor.email},
+				{selector: 'span#mdb-contractor-address', innerHTML: event.contractor.address},
+				{selector: 'p#mdb-notes', innerHTML: event.invoice.notes},
+				{selector: 'span#mdb-phone', innerHTML: event.contractor.phone},
+				{selector: 'span#mdb-email', innerHTML: event.contractor.email},
+				{selector: 'span#mdb-address', innerHTML: event.contractor.address},
 			]
 
-			try {
-				instructions.forEach((i) => {
+			instructions.forEach((i) => {
+				try {
 					document.querySelector(i.selector).innerHTML = i.innerHTML
-				})
+				} catch (err) {
+					console.error(`An error occurred while attempting to change the contents of element at location: "${i.selector}" with the content: \n\n ${i.innerHTML} \n${err}.`)
+				}
+			})
+
+			try {
+				document.querySelector('div.table-responsive').remove()
 			} catch (err) {
-				console.error(`An error was thrown: ${err}.`)
+				console.error(`An error occurred while attempting to remove elements from the document: ${err}`)
+			}
+
+			try {
+				// let sections = {
+				// 	materials: event.materials,
+				// 	labor: event.labor,
+				// }
+
+				var sections = getElementById('div#mdb-sections')
+				var foo = document.createElement('h2')
+				foo.innerHTML = 'blatherskite'
+
+				sections.appendChild(foo)
+
+				// for (const s in sections) {
+				// 	let section = document.createElement('div')
+				// 	section.setAttribute('id', `mdb-${s}`)
+				// 	section.setAttribute('class', 'table-responsive')
+					
+				// 	let table = document.createElement('table')
+				// 	table.setAttribute('class', 'table mb-0 talbe-striped invoice-table')
+
+				// 	let thead = document.createElement('thead')
+				// 	thead.setAttribute('class', 'bg-active')
+
+				// 	let tr = document.createElement('tr')
+				// 	tr.setAttribute('class', 'tr')
+
+				// 	let td = document.createElement('td')
+				// 	td.innerHTML = 'foo'
+
+				// 	tr.append(td)
+				// 	thead.append(tr)
+				// 	table.append(thead)
+				// 	section.append(table)
+				// 	document.getElementById('#mdb-sections').appendChild(section)
+				// }
+
+			} catch (err) {
+				console.err(`An error occurred while attempting to add elements into the document: ${err}`)
 			}
 		}, event)
 
@@ -86,7 +132,7 @@ export const handler = async (event) => {
 
 if (!region) {
 	const document_type = 'invoice'
-	const path = `api/${document_type}.json`
+	const path = `../api/${document_type}.json`
 
 	fs.readFile(path, 'utf8', (err, file) => {
 		if (err) {
